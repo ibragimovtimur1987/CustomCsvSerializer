fe52f7563be298acbe4e5a532d314a80941bca7c
Тимур <t.ibragimov@systtech.ru>
2025-03-17 11:18:05 +0500

    refact

--- Program.cs
+++ Program.cs
@@ -3,61 +3,67 @@ using System.Management;
 using CustomCsvSerializer;
 using Newtonsoft.Json;
 
-GetEnvInfo();
-
-var testObject = F.Get();
-int iterations = 100000;
-
-TestSerializeDeserialize("Мой рефлекшен:", iterations, testObject, CsvSerializer.Serialize, CsvSerializer.Deserialize<F>);
-TestSerializeDeserialize("Cтандартный механизм (NewtonsoftJson):", iterations, testObject, JsonConvert.SerializeObject, JsonConvert.DeserializeObject<F>);
-
-void GetEnvInfo()
+class Program
 {
-    Console.WriteLine("Версия ОС: " + Environment.OSVersion);
-    Console.WriteLine("Количество процессоров: " + Environment.ProcessorCount);
-    
-    var searcherCpu = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
-    foreach (var o in searcherCpu.Get())
+    static void Main(string[] args)
     {
-        var obj = (ManagementObject)o;
-        Console.WriteLine("Процессор: " + obj["Name"]);
+        GetEnvInfo();
+
+        var testObject = F.Get();
+        int iterations = 100000;
+
+        TestSerializeDeserialize("Мой рефлекшен:", iterations, testObject, CsvSerializer.Serialize, CsvSerializer.Deserialize<F>);
+        TestSerializeDeserialize("Cтандартный механизм (NewtonsoftJson):", iterations, testObject, JsonConvert.SerializeObject, JsonConvert.DeserializeObject<F>);
     }
     
-    var searcherMemory = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
-    ulong totalMemory = 0;
-    foreach (var o in searcherMemory.Get())
+    static void GetEnvInfo()
     {
-        var obj = (ManagementObject)o;
-        totalMemory += (ulong)obj["Capacity"];
-    }
-
-    Console.WriteLine("Объем оперативной памяти: " + totalMemory / (1024 * 1024) + " МБ");
-}
-
-void TestSerializeDeserialize(string nameTest, int iterations1, F testObject1, Func<F, string> funcSerialize, Func<string, F> funcDeserialize)
-{
-    Console.WriteLine(nameTest);
+        Console.WriteLine("Версия ОС: " + Environment.OSVersion);
+        Console.WriteLine("Количество процессоров: " + Environment.ProcessorCount);
     
-    Stopwatch stopwatch = new Stopwatch();
+        var searcherCpu = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+        foreach (var o in searcherCpu.Get())
+        {
+            var obj = (ManagementObject)o;
+            Console.WriteLine("Процессор: " + obj["Name"]);
+        }
+    
+        var searcherMemory = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+        ulong totalMemory = 0;
+        foreach (var o in searcherMemory.Get())
+        {
+            var obj = (ManagementObject)o;
+            totalMemory += (ulong)obj["Capacity"];
+        }
 
-    stopwatch.Start();
-
-    for (int i = 0; i < iterations1; i++)
-    {
-        funcSerialize(testObject);
+        Console.WriteLine("Объем оперативной памяти: " + totalMemory / (1024 * 1024) + " МБ");
     }
 
-    stopwatch.Stop();
-
-    string data = funcSerialize(testObject1);
-    Console.WriteLine($"Время на сериализацию {data}: {stopwatch.ElapsedMilliseconds} мс");
-
-    stopwatch.Restart();
-    for (int i = 0; i < iterations1; i++)
+    static void TestSerializeDeserialize(string nameTest, int iterations1, F testObject, Func<F, string> funcSerialize, Func<string, F> funcDeserialize)
     {
-        funcDeserialize(data);
-    }
+        Console.WriteLine(nameTest);
+    
+        Stopwatch stopwatch = new Stopwatch();
 
-    stopwatch.Stop();
-    Console.WriteLine($"Время на десериализацию : {stopwatch.ElapsedMilliseconds} мс");
+        stopwatch.Start();
+
+        for (int i = 0; i < iterations1; i++)
+        {
+            funcSerialize(testObject);
+        }
+
+        stopwatch.Stop();
+
+        string data = funcSerialize(testObject);
+        Console.WriteLine($"Время на сериализацию {data}: {stopwatch.ElapsedMilliseconds} мс");
+
+        stopwatch.Restart();
+        for (int i = 0; i < iterations1; i++)
+        {
+            funcDeserialize(data);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Время на десериализацию : {stopwatch.ElapsedMilliseconds} мс");
+    }
 }
